b9160fd724bb9a4ab2c20396770caefc63d0c524
ravikr126 <ravi14577@gmail.com>
2024-09-07 15:52:44 +0530

    adding user condition , creating account and cookies

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,90 @@
-import React from 'react'
-import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
 import { Icons } from './icons'
 import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import { cookies } from 'next/headers'
 
-const Navbar = () => {
-  return (
-    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16 '>
-        
-        <header className="relative bg-white">
-           <MaxWidthWrapper>
-            <div className="border-b border-gray-200">
-                <div className='flex h-16 items-center'>
-                    {/* todo mobile nav */}
-                    <div className="ml-4 flex lg:ml-0">
-                        <Link href='/'>
-                        <Icons.logo className='h-10 w-10'/>
-                        </Link>
-                    </div>
-                    <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
-                        <NavItems/>
-                    </div>
+const Navbar = async () => {
+    const nextCookies = cookies()
+    const user=null
+  
+    return (
+      <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+        <header className='relative bg-white'>
+          <MaxWidthWrapper>
+            <div className='border-b border-gray-200'>
+              <div className='flex h-16 items-center'>
+             
+  
+                <div className='ml-4 flex lg:ml-0'>
+                  <Link href='/'>
+                    <Icons.logo className='h-10 w-10' />
+                  </Link>
                 </div>
+  
+                <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                  <NavItems />
+                </div>
+  
+                <div className='ml-auto flex items-center'>
+                  <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                    {user ? null : (
+                      <Link
+                        href='/sign-in'
+                        className={buttonVariants({
+                          variant: 'ghost',
+                        })}>
+                        Sign in
+                      </Link>
+                    )}
+  
+                    {user ? null : (
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    )}
+  
+                    {user ? (
+            <span>user accound components add here</span>
+                    ) : (
+                      <Link
+                        href='/sign-up'
+                        className={buttonVariants({
+                          variant: 'ghost',
+                        })}>
+                        Create account
+                      </Link>
+                    )}
+  
+                    {user ? (
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    ) : null}
+  
+                    {user ? null : (
+                      <div className='flex lg:ml-6'>
+                        <span
+                          className='h-6 w-px bg-gray-200'
+                          aria-hidden='true'
+                        />
+                      </div>
+                    )}
+  
+                    <div className='ml-4 flow-root lg:ml-6'>
+                      Cart components
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-           </MaxWidthWrapper>
+          </MaxWidthWrapper>
         </header>
-    </div>
-  )
-}
-
-export default Navbar
+      </div>
+    )
+  }
+  
+  export default Navbar
